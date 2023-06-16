8b1bb69155775c965fadc93d8482cb7510bb4d20
Рустам Мулюков <rustammul@mail.ru>
2023-06-16 11:31:33 +0500

    16.06

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+
 from dotenv import load_dotenv
 from pathlib import Path
 
